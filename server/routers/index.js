014b69720b108f39f26dde35a8952536b101734e
tsinglemon <tsinglemon@126.com>
2018-06-25 10:13:18 +0800

    删掉之前临时建的文件

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,12 +5,9 @@ const router = express.Router();
 
 const user = require('./user.js');
 const habit = require('./habit.js');
-const chat = require('./chat.js');
-const house = require('./house.js');
 
 
 router.use('/user',user)
 router.use('/habit',habit)
-router.use('/house',house)
 
 module.exports=router;
